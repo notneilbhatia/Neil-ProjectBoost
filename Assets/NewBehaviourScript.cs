5428c0d8755ea0efb43c7f2da97733ef792738fe
Neil <neil.bhatia2002@gmail.com>
2020-05-29 13:41:36 +0800

    Black Screen on Object Collision
    Whenever the rocket collisdes with an object, instead of loading the next level I get a black screen.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,78 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+    Rigidbody rigidBody;
+	AudioSource thrust;
+   [SerializeField] float rcsThrust = 100f;
+   [SerializeField] float mainThrust = 100f;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		thrust = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Fly();
+        Rotate();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Respawn":
+                {
+                    print("Good Luck");
+                    break;
+                }
+
+            case "Finish":
+                {
+                    SceneManager.LoadScene(1);
+                    break;
+                }
+                
+            default:
+                { 
+                SceneManager.LoadScene(0);
+                break;
+        }
+        }
+    }
+
+    private void Fly()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+
+            if (!thrust.isPlaying)
+            {
+                thrust.Play();
+            }
+        }
+        else
+        {
+            thrust.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+        float rotationthisframe = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward*rotationthisframe);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward*rotationthisframe);
+        }
+        rigidBody.freezeRotation = false;
+    }
+}
