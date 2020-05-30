529364c39eb90f43ac92e06d99f9459032395a6c
Neil <neil.bhatia2002@gmail.com>
2020-05-30 14:35:12 +0800

    TheOscilatorisBack

--- Assets/Oscilator.cs
+++ Assets/Oscilator.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[DisallowMultipleComponent]
+public class Oscilator : MonoBehaviour
+{
 
-public class Oscilator : MonoBehaviour {
+	[SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+	[SerializeField] float period = 2f;
 
-	// Use this for initialization
-	void Start () {
-		
+	[Range(0, 1)] [SerializeField] float movementFactor;
+
+	Vector3 startingPos;
+
+	void Start()
+	{
+		startingPos = transform.position;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		if (period <= Mathf.Epsilon) { return; }
+		float cycles = Time.time / period;
+
+		const float tau = Mathf.PI;
+		float rawSinWave = Mathf.Sin(cycles * tau);
+
+		movementFactor = rawSinWave / 2f + 0.5f;
+		Vector3 offset = movementFactor * movementVector;
+		transform.position = startingPos + offset;
+
 	}
 }
